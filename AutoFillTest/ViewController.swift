b86f71baf392093e2a84d3b1e4e6ce5ff00538e1
Valentine Silvansky <v.silvansky@gmail.com>
2022-05-19 16:09:32 +0300

    Initial Commit

---
+++ AutoFillTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoFillTest
+//
+//  Created by Valentine Gorshkov on 19.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
